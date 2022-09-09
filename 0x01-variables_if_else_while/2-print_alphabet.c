22794682a8e43dbdb4390ebc5a71c09a5174d418
Chigbogwu <chejieh.chigbogwu@yahoo.com>
2022-09-09 07:39:59 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	char alphabet;
-	
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 		putchar('\n');
 
