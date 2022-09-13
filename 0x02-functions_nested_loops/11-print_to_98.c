13fbf82bb0387bca63ccea85a021c56fb5bb85c0
Chigbogwu <chejieh.chigbogwu@yahoo.com>
2022-09-13 14:00:19 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98
+ *
+ * @n: The number to begin counting at
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while ( n > 98)
+			printf("%d, ", n--);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
