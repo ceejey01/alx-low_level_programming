a01a5031803f8ffb5ebab11d0746b385428b4943
Chigbogwu <chejieh.chigbogwu@yahoo.com>
2022-09-22 01:24:47 -0700

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,6 +5,7 @@
  * @n: input value
  * Return: n value
  */
+
 char *leet(char *n)
 {
 	int i, j;
@@ -12,15 +13,11 @@ char *leet(char *n)
 	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
-
 	{
 		for (j = 0; j < 10; j++)
-		
 		{
 			if (n[i] == s1[j])
-			
 			{
-			
 				n[i] = s2[j];
 			
 			}
