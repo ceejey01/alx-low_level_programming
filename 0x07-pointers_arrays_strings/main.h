0358372ced9a993f36684e85463245210bb28d27
Chigbogwu <chejieh.chigbogwu@yahoo.com>
2022-09-26 10:29:42 -0700

    Task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H
+
 int _putchar(char);
-char *_memset(char 8s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -8,3 +11,5 @@ char *strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **ss, char *to);
+
+#endif
